f05069baf8506f17ba248bc3e772e4e8af47ca23
Ziyad Sameh <ziyad.ss@hotmail.com>
2022-07-10 23:40:51 +0200

    Redone 01-16

--- 01 Introductory Problems/16-apple-divison.cpp
+++ 01 Introductory Problems/16-apple-divison.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include <vector>
 
+unsigned int solve(const std::vector<unsigned int> &p, unsigned long long group1 = 0, unsigned long long group2 = 0, size_t idx = 0)
+{
+    if (p.size() == idx)
+        return llabs(group1 - group2);
+
+    unsigned int x = p[idx];
+    unsigned int y = solve(p, group1 + x, group2, idx + 1);
+    unsigned int z = solve(p, group1, group2 + x, idx + 1);
+    return std::min(y, z);
+}
+
 int main()
 {
-    int n;
+    unsigned int n;
     std::cin >> n;
 
-    long long sum = 0;
-    std::vector<int> p(n);
-    for (auto &&p_i : p)
-    {
+    std::vector<unsigned int> p(n);
+    for (auto &p_i : p)
         std::cin >> p_i;
-        sum += p_i;
-    }
 
-    long long half = 0;
-    int cases = 1 << n;
-    for (int i = 0; i < cases; i++)
-    {
-        long long g = 0;
-        for (int j = 0; j < n; j++)
-            if (i & (1 << j))
-                g += p[j];
+    std::cout << solve(p);
 
-        if (g <= sum / 2)
-            half = std::max(g, half);
-    }
-
-    std::cout << sum / 2 - half;
     return 0;
 }
